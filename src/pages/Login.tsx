ab04de5e8b8a91ba136c7de8bfcf1197cf890f06
Kamrul Hasan <kamrul.hasan.cse18344@gmail.com>
2025-07-20 01:07:52 +0600

    Tech net redux project for practice

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Login Page</h1>
+      <p className="mt-4">Please enter your credentials to log in.</p>
+      {/* Add login form here */}
+    </div>
+  );
+};
+
+export default Login;
